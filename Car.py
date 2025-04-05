4ded0b95852ebeda5640492b4976347e31167d42
ParkerPrograms <njparker2@students.unwsp.edu>
2025-04-04 22:30:44 -0500

    Add files via upload

--- Car.py
+++ Car.py
@@ -1,14 +1,58 @@
-# Program # 2: Car Class
-# Write a class named Car that has the following data attributes:
+# Nathan Parker
+# 04/04/25
+# Program #2: Car Class
 
-# __year_model (for the car's year model)
-# __make (for the make of the car)
-# __speed (for the car's current speed)
-# The Car class should have an __init__ method that accepts the car's year model and make as arguments.  These values should be assigned to the object's __year_model and __make data attributes.  It should also assign 0 to the __speed data attribute.
 
-# The class should also have the following methods:
 
-# The accelerate method should add 5 to the speed data attribute each time it it called.
-# The brake method should subtract 5 from the speed data attribute each time it is called.
-# The get_speed method should return the current speed.
-# Next, design a program that creates a Car object then calls the accelerate method five times.  After each call to the accelerate method, get the current speed of the car and display it.  The call the brake method.  After each call to the brake method, get the current speed of the car and display it.
+# Create a class
+class Car:
+
+    # The __init__ method accepts year_model and make and assigns them to data attributes
+    # The __init__ method also creates the speed attribute and sets it to 0
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+
+    # The accelerate method adds 5 to the speed
+    def accelerate(self):
+        self.__speed += 5
+
+    # The brake method subtracts 5 from the speed
+    def brake(self):
+        self.__speed -= 5
+
+    # The get_speed method returns the speed
+    def get_speed(self):
+        return self.__speed
+
+
+
+# define the main function
+def main():
+
+    # Get the year and model from the user
+    car_year_model = input('Enter the year and model of the car: ')
+
+    # Get the make from the user
+    car_make = input('Enter the make of the car: ')
+
+    # Create an object from the car class
+    car = Car(car_year_model, car_make)
+
+    # Call the accelerate method and the get_speed method 5 times
+    print('Accelerating...')
+    for loop in range(5):
+        car.accelerate()
+        print(f'The current speed is {car.get_speed()}.')
+
+    # Call the brake method and the get_speed method 5 times
+    print('Braking...')
+    for loop in range(5):
+        car.brake()
+        print(f'The current speed is {car.get_speed()}.')
+
+# Call the main function
+if __name__ == '__main__':
+    main()
+
--- Employee.py
+++ Employee.py
@@ -1,10 +1,74 @@
-# Program #4 Employee Class:
-# Write a class Employee that holds the following data about an employee in attributes: name, ID number, department, and job title.
+# Nathan Parker
+# 04/04/25
+# Program #4 Employee Class
 
-# Once you have written the class, write a program that creates three Employee objects to hold the following data.
 
-# Name	ID Number	Department	Job Title
-# Susan Meyers	47899 	Accounting	Vice President
-# Mark Jones	39119	IT	Programmer
-# Joy Rogers	81774	Manufacturing	Engineer
-# The program should store the data in the three objects, then display the data for each employee on the screen.
+
+# Create a class
+class Employee:
+
+    # The __init__ method accepts name, ID_number, department, and job_title and assigns them to data attributes
+    def __init__(self, name, ID_number, department, job_title):
+        self.__name = name
+        self.__ID_number = ID_number
+        self.__department = department
+        self.__job_title = job_title
+
+    # The set_name method sets the __name instance attribute to the name
+    def set_name(self, name):
+        self.__name = name
+
+    # The set_ID_number method sets the __ID_number instance attribute to the ID_number
+    def set_ID_number(self, ID_number):
+        self.__ID_number = ID_number
+
+    # The set_department method sets the __department instance attribute to the department
+    def set_department(self, department):
+        self.__department = department
+
+    # The set_job_title method sets the __job_title instance attribute to the job_title
+    def set_job_title(self, job_title):
+        self.__job_title = job_title
+
+    # The get_name method returns the value referenced by __name
+    def get_name(self):
+        return self.__name
+
+    # The get_ID_number method returns the value referenced by __ID_number
+    def get_ID_number(self):
+        return self.__ID_number
+
+    # The get_department method returns the value referenced by __department
+    def get_department(self):
+        return self.__department
+
+    # The get_job_title method returns the value referenced by __job_title
+    def get_job_title(self):
+        return self.__job_title
+
+
+
+# define the main function
+def main():
+
+    # Create an object from the Employee class
+    employee_1 = Employee('Susan Meyers', '47899', 'Accounting', 'Vice President')
+
+    # Create a second object from the Employee class
+    employee_2 = Employee('Mark Jones', '39119', 'IT', 'Programmer')
+
+    # Create a third object from the Employee class
+    employee_3 = Employee('Joy Rogers', '81774', 'Manufacturing', 'Engineer')
+
+    # Display the attributes of the objects
+    for employee_num in (employee_1,employee_2,employee_3):
+        print(f'''
+Name: {employee_num.get_name()}
+ID Number: {employee_num.get_ID_number()}
+Department: {employee_num.get_department()}
+Job Title: {employee_num.get_job_title()}''')
+
+# Call the main function
+if __name__ == '__main__':
+    main()
+
